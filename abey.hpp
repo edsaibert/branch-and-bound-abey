bf5e255ad90a49f65a942ed4e6b4ef9e9511677b
Caio de Miranda <cefm23@inf.ufpr.br>
2026-05-29 00:38:52 -0300

    assinatura de funcao corrigida dnv

--- abey.hpp
+++ abey.hpp
@@ -23,7 +23,7 @@ Leilao abey_cria_leilao(int p, int c);
 
 Ofertas abey_cria_ofertas(int qtd_ofertas);
 
-void abey_bnb_max_ganho(Leilao leilao, int l, int (*bound)(Leilao, Leilao), vector<int>& res);
+void abey_bnb_max_ganho(Leilao leilao, int l, int (*bound)(Leilao, Leilao), vector<int>& opt_x, int& opt_p);
 
 int abey_bound_prof(Leilao arremts, Leilao prods_pend);
 
